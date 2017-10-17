5fb1ab45cca3256838ecbceb9a3ee346e9f1c646
Rakamishu <mkrotify@gmail.com>
2017-10-17 04:09:27 +0300

    mail

---
+++ App/Core/Mail.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Core;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class Mail
+{
+    public function send($to, $subject, $message)
+    {
+        $mail = new PHPMailer(true);
+        try {
+            //Server settings
+            $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+            $mail->isSMTP();                                      // Set mailer to use SMTP
+            $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true;                               // Enable SMTP authentication
+            $mail->Username = 'mihailbusnss@gmail.com';                 // SMTP username
+            $mail->Password = '';                           // SMTP password
+            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = 587;                                    // TCP port to connect to
+            $mail->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+            
+
+            //Recipients
+            $mail->setFrom('mihailbusnss@gmail.com', 'Admin');
+            $mail->addAddress($to);               // Name is optional
+
+            //Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = $subject;
+            $mail->Body    = $message;
+
+            $mail->send();
+        } catch (Exception $e) {
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        }
+    }
+
+}
+
